d9edb73399690b9ee06b9fba1fd74b078a5b4d3e
vibhor <vibhor27061995@gmail.com>
2023-12-17 12:20:36 +0530

    Components commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import {Switch, Route, Link} from 'react-router-dom';
+import { Layout, Typography, Space, Menu } from 'antd';
+
+import {Navbar, Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails  } from './components';
+import './App.css';
+import { BulbOutlined, FundOutlined, HomeOutlined, MoneyCollectFilled, MoneyCollectOutlined } from '@ant-design/icons';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <div className='app'>
+        <div className='navbar'>
+            <Navbar />
+        </div>
+        <div className='main'>
+            <Layout>
+                <div className='routes'>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Homepage />
+                        </Route>
+                        <Route exact path='/exchanges'>
+                            <Exchanges />
+                        </Route>
+                        <Route exact path='/cryptocurrencies'>
+                            <Cryptocurrencies />
+                        </Route>
+                        <Route exact path='/crypto/:coinId'>
+                            <CryptoDetails />
+                        </Route>
+                        <Route exact path='news'>
+                            <News />
+                        </Route>
+                    </Switch>
+                </div>
+            </Layout>
+
+        </div>
+        <div className='footer'>
+
+        </div>
+        
+    </div>
+  );
 }
 
 export default App
